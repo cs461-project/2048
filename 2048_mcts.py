41773d281aeed910952ae9fb097a2f4842bd2ba6
Berk Çakar <berk.cakar@ug.bilkent.edu.tr>
2023-12-04 02:42:57 +0300

    Implement MCTS from scratch

--- 2048_mcts.py
+++ 2048_mcts.py
@@ -1,22 +1,42 @@
+import numpy as np
+import random
+import math
+from copy import deepcopy
 from distutils.util import strtobool
 import time
 import gymnasium as gym
-import gym_game2048
 from gym_game2048.wrappers import PreprocessForTensor, Normalize2048, RewardConverter, RewardByScore
 from gymnasium.wrappers import FlattenObservation, TimeLimit, TransformReward
-
-from mcts_general.agent import MCTSAgent
-from mcts_general.config import MCTSAgentConfig
-from mcts_general.game import DiscreteGymGame
-
+import matplotlib.pyplot as plt
 import numpy as np
 import pygame
-
 import argparse
 import os
-
 import warnings
 warnings.filterwarnings('ignore')
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py") + f"_{int(time.time())}",
+        help="the name of this experiment")
+    parser.add_argument("--goal", type=int, default=int(np.power(2, 32)), help="the goal of the game, note that the game will end when the goal is reached")
+    parser.add_argument("--seed", type=int, default=1, help="seed of the experiment")
+    parser.add_argument("--env-id", type=str, default="gym_game2048/Game2048-v0", help="the id of the gymnasium environment")
+    parser.add_argument("--render-mode", type=str, default="human", help="the mode to render the next_state", choices=["human", "human_only", "terminal"])
+    parser.add_argument("--num-episodes", type=int, default=10, help="the number of episodes to run")
+    parser.add_argument("--save-logs", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True, help="whether to save logs into the `runs/{run_name}` folder")
+    parser.add_argument("--save-models", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True, help="whether to save models into the `runs/{run_name}` folder")
+    parser.add_argument("--load-model", type=str, default=None, help="the path to load the model from")
+    parser.add_argument("--n", type=int, default=4, help="the size of the board")
+    parser.add_argument("--mcts-simulations", type=int, default=400, help="the number of simulations to run for each step, i.e., the depth of the tree")
+
+    args = parser.parse_args()
+
+    global size, render_mode
+    size = args.n
+    render_mode = args.render_mode
+
+    return args
 
 window = None
 window_title = "CS 461 - Term Project (Group 12) - 2048 (w/ MCTS)"
@@ -153,29 +173,6 @@ def render(board, score, best_score, max_tile, episode=0, step=0, render_mode="h
         print("==============================")
         print(f"Episode: {episode} | Step: {step} | Score: {score} | Max Tile: {max_tile} | Time Elapsed: {time.time() - start_time:.2f}s\n")
 
-def parse_args():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--exp-name", type=str, default=os.path.basename(__file__).rstrip(".py") + f"_{int(time.time())}",
-        help="the name of this experiment")
-    parser.add_argument("--goal", type=int, default=int(np.power(2, 32)), help="the goal of the game, note that the game will end when the goal is reached")
-    parser.add_argument("--seed", type=int, default=1, help="seed of the experiment")
-    parser.add_argument("--env-id", type=str, default="gym_game2048/Game2048-v0", help="the id of the gymnasium environment")
-    parser.add_argument("--render-mode", type=str, default="human", help="the mode to render the observation", choices=["human", "human_only", "terminal"])
-    parser.add_argument("--num-episodes", type=int, default=10, help="the number of episodes to run")
-    parser.add_argument("--save-logs", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True, help="whether to save logs into the `runs/{run_name}` folder")
-    parser.add_argument("--save-models", type=lambda x: bool(strtobool(x)), default=True, nargs="?", const=True, help="whether to save models into the `runs/{run_name}` folder")
-    parser.add_argument("--load-model", type=str, default=None, help="the path to load the model from")
-    parser.add_argument("--n", type=int, default=4, help="the size of the board")
-    parser.add_argument("--mcts-simulations", type=int, default=400, help="the number of simulations to run for each step, i.e., the depth of the tree")
-
-    args = parser.parse_args()
-
-    global size, render_mode
-    size = args.n
-    render_mode = args.render_mode
-
-    return args
-
 def make_env(env_id, window_title, seed, args):
     def _thunk():
         # TODO: Check if capturing a video is necessary
@@ -202,18 +199,134 @@ def make_env(env_id, window_title, seed, args):
 
     return _thunk
 
+class Normalizer:
+    def __init__(self):
+        self.max = -float("inf")
+        self.min = float("inf")
+
+    def update(self, value):
+        self.max = max(self.max, value)
+        self.min = min(self.min, value)
+
+    def normalize(self, value):
+        if self.max > self.min:
+            return (value - self.min) / (self.max - self.min)
+        return value
+
+class DeepCopyWrapper(gym.Wrapper):
+    def __deepcopy__(self, memo={}):
+        new_instance = self.__class__
+        result = new_instance.__new__(new_instance)
+        memo[id(self)] = result
+        for key, value in self.__dict__.items():
+            setattr(result, key, deepcopy(value, memo))
+        return result
+
+class DeepCopyEnv():
+    def __init__(self, env):
+        self.env = DeepCopyWrapper(env) if not isinstance(env, DeepCopyWrapper) else env
+
+    def reset(self):
+        return self.env.reset()
+
+    def close(self):
+        self.env.close()
+
+    def step(self, action):
+        next_state, reward, is_done, _, info = self.env.step(action)
+        return next_state, reward, is_done, _, info
+
+    def get_copy(self):
+        return DeepCopyEnv(deepcopy(self.env))
+
+    def legal_actions(self):
+        return [i for i in range(self.env.action_space.n)]
+
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.number_of_visits = 0
+        self.value_sum = 0.0
+        self.next_state = None
+        self.reward = 0
+        self.is_done = False
+        self.deep_copy = None
+
+    @property
+    def expanded(self) -> bool:
+        return len(self.children) > 0
+
+    @property
+    def mean_value(self) -> float:
+        return 0 if self.number_of_visits == 0 or self.is_done else self.value_sum / self.number_of_visits
+
+    def expand_node(self, next_state, reward, is_done, deep_copy, initial_number_of_visits=0):
+        self.next_state = next_state
+        self.reward = reward
+        self.is_done = is_done
+        self.deep_copy = deep_copy
+        self.number_of_visits = initial_number_of_visits
+        for action in deep_copy.legal_actions():
+            self.children[action] = Node()
+
+def ucb_score(parent, child, normalizer):
+    return ((2 * (math.sqrt(parent.number_of_visits) / (child.number_of_visits + 1))) * (1 / len(parent.children))) + (normalizer.normalize(child.reward + child.mean_value) if child.number_of_visits > 0 else 0)
+
+def backpropagate(path, value, normalizer):
+    for node in reversed(path):
+        node.number_of_visits += 1
+        node.value_sum += value
+        normalizer.update(node.reward + node.mean_value)
+        value = node.reward + value
+
+def child_select(node, normalizer):
+    max_ucb = max(ucb_score(node, child, normalizer) for action, child in node.children.items())
+    action = random.choice([action for action, child in node.children.items() if ucb_score(node, child, normalizer) == max_ucb])
+    return action, node.children[action]
+
+def action_select(node):
+    number_of_visits = np.array([child.number_of_visits for action, child in node.children.items()])
+    actions = np.array([action for action, child in node.children.items()])
+    return actions[np.argmax(number_of_visits)]
+
+def mcts_run(next_state, reward, is_done, env, num_of_simulations):
+        root = Node()
+        reward = reward
+        root.expand_node(next_state, reward, is_done, env.get_copy(), initial_number_of_visits=0)
+
+        normalizer = Normalizer()
+
+        for i in range(num_of_simulations):
+            node = root
+            path = [node]
+
+            while node.expanded:
+                action, node = child_select(node, normalizer)
+                path.append(node)
+
+            parent = path[-2]
+            if not parent.is_done:
+                deep_copy = parent.deep_copy.get_copy()
+                next_state, reward, is_done, _, info = deep_copy.step(action)
+                value = reward
+                node.expand_node(next_state, reward, is_done, deep_copy, initial_number_of_visits=0)
+            else:
+                value = 0
+
+            backpropagate(path, value, normalizer)
+
+        return root
+
+def mcts_step(env, next_state, reward, is_done, num_of_simulations):
+    result_node = mcts_run(next_state, reward, is_done, env, num_of_simulations)
+    return action_select(result_node)
+
 if __name__ == "__main__":
     args = parse_args()
     rm = args.render_mode
+    num_of_simulations = args.mcts_simulations
 
-    config = MCTSAgentConfig()
-    config.num_simulations = args.mcts_simulations
-    agent = MCTSAgent(config)
-
-    if args.load_model is not None:
-        agent = agent.load(args.load_model)
-
-    game = DiscreteGymGame(make_env(args.env_id, window_title, args.seed, args)(), seed=args.seed)
+    env = DeepCopyEnv(make_env(args.env_id, window_title, args.seed, args)())
     episode_scores = []
     episode_max_tiles = []
 
@@ -221,8 +334,8 @@ if __name__ == "__main__":
 
     for i in range (1, args.num_episodes + 1):
         print(f"Episode {i} started.\n")
-        state = game.reset()
-        done = False
+        state = env.reset()
+        is_done = False
         reward = 0
 
         step = 0
@@ -230,12 +343,11 @@ if __name__ == "__main__":
         scores = []
         maximum_tiles = []
 
-        while not done:
+        while not is_done:
             step += 1
 
-            action = agent.step(game, state, reward, done)
-            state, reward, done, info = game.step(action)
-
+            action = mcts_step(env, state, reward, is_done, num_of_simulations)
+            state, reward, is_done, _, info = env.step(action)
             score = info["score"]
             max_tile = 2 ** info["max"]
 
@@ -255,5 +367,4 @@ if __name__ == "__main__":
         if args.save_models or args.save_logs:
             os.makedirs(f"runs/{args.exp_name}", exist_ok=True)
             # TODO: Implement a proper save function, if the model supports it
-            # It seems like the model recreates the tree every time, so loading the model does not make sense
             # agent.save(f"runs/{args.exp_name}/{os.path.basename(__file__).rstrip('.py')}_episode_{i}.pkl")
